18d0bace093ff8e8b6e437a4d4100a66867e4610
Alvaro Diaz Valenzuela <alvaro@efaa.cl>
2024-01-07 13:28:03 -0300

    Hide secrets.

--- pyqcf/dependencies.py
+++ pyqcf/dependencies.py
@@ -5,17 +5,13 @@ Conexión con Mongo.
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from pymongo import database as mdb
+import keyring
 
 
 def get_db() -> mdb:
-<<<<<<< HEAD
-    uri = "mongodb+srv://adiazv:rLpbgvPkiAcvqhrN@r07.7dkjtpy.mongodb.net/?retryWrites=true&w=majority"
-=======
-    # Keyring must be installed in the python environment
     user = "adiazv"
     pwd = keyring.get_password("mongo-diego", user)
     uri = f"mongodb+srv://{user}:{pwd}@r07.7dkjtpy.mongodb.net/?retryWrites=true&w=majority"
->>>>>>> 687f86a (fixup! Resuelve tarjeta https://trello.com/c/ysdbSVt6)
     # Set the Stable API version when creating a new client
     client = MongoClient(uri, server_api=ServerApi('1'))
 
